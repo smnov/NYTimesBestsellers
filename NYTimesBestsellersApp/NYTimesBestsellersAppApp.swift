f0667e99de78c8ce4ebb9ebfc11696edf721f12d
aleksandr semenov <aleksandr.semenovsky@gmail.com>
2024-01-17 15:23:11 +0800

    Initial Commit

---
+++ NYTimesBestsellersApp/NYTimesBestsellersAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTimesBestsellersAppApp.swift
+//  NYTimesBestsellersApp
+//
+//  Created by Александр Семенов on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct NYTimesBestsellersAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
